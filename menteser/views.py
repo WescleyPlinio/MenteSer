20432f037b0286de58581851ec95c4a394e4f038
Wescley <wescleygaldino06@gmail.com>
2025-04-11 17:42:18 -0300

    Iniciando projeto django

---
+++ menteser/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View 
+
+# Index page
+class Index(View):
+    def get(self, request):
+        return render(request, 'index.html')
