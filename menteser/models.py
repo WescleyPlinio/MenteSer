95ce8c5f0b522a9ed9dbab48ccbdf316556b71df
Wescley <wescleygaldino06@gmail.com>
2025-04-25 15:24:58 -0300

    Making model and fixtures of "tratamentos"

--- menteser/models.py
+++ menteser/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Transtorno(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao_tratamento = models.TextField(max_length=500)
+    descricao = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.nome
